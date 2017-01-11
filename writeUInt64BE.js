d4d094826e9c58068a414eb164ef0b3bbef4bb03
oconnorct1 <oconnorct1@gmail.com>
2017-01-11 13:50:20 -0500

    finished

---
+++ writeUInt64BE.js
@@ -0,0 +1,28 @@
+'use strict';
+const Buffer = require('buffer').Buffer;
+
+// writeInt32BE(value, number[, offest])
+
+function writeUInt64BE(buf, int64, offset) {
+  if (typeof int64 !== 'number'){
+    int64 = Number(int64);
+  }
+
+  const MAX_UINT32 = 0xFFFFFFFF; // 64 bits / 8 bytes.
+  // write
+  const big = ~~(int64 / MAX_UINT32); // ~~ is equivalent to Math.floor()
+  const low = (int64 % MAX_UINT32) - big;
+
+  if (!offset) {
+    const b = new Buffer(8);
+    b.writeUInt32BE(big, 0);
+    b.writeUInt32BE(low, 4);
+    return Buffer.concat([buf, b]);
+  } else {
+    buf.writeUInt32BE(big, offset);
+    buf.writeUInt32BE(low, offset + 4);
+    return buf;
+  }
+}
+
+module.exports = writeUInt64BE;
